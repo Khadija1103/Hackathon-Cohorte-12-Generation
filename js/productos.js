f70d5e5b6e2e692419f9cdc71458db19b92f3917
alanalozano1988-coder <alanalozano1988@gmail.com>
2026-06-19 16:44:44 -0500

    subo enlaces para productos de carrito de compras

--- js/productos.js
+++ js/productos.js
@@ -36,3 +36,144 @@ const productos = [
     imagen: "https://images.unsplash.com/photo-1523170335258-f5ed11844a49",
   },
 ];
+
+
+const productos2= [
+  {
+    id: 1,
+    nombre: "Camiseta Oversize",
+    precio: 65000,
+    imagen: "https://img.kwcdn.com/product/fancy/b0a29e77-5619-4ee2-a0bb-ffc52b0f2361.jpg?imageView2/2/w/800/q/70/format/avif",
+  },
+  {
+    id: 2,
+    nombre: "Chaqueta Denim",
+    precio: 120000,
+    imagen: "https://img.kwcdn.com/product/fancy/4ec1d298-5683-479e-bdc7-cc43dac1e853.jpg?imageView2/2/w/800/q/70/format/avif",
+  },
+  {
+    id: 3,
+    nombre: "Tenis Urbanos",
+    precio: 180000,
+    imagen: "hhttps://img.kwcdn.com/product/fancy/a3bfd635-7607-4b59-81c0-54bf72200538.jpg?imageView2/2/w/800/q/70/format/avif",
+  },
+  {
+    id: 4,
+    nombre: "Bolso Casual",
+    precio: 85000,
+    imagen: "https://img.kwcdn.com/product/fancy/3f3127fd-2cc0-4faf-bb20-d1486c4e0a9c.jpg?imageView2/2/w/800/q/70/format/avif",
+  },
+  {
+    id: 5,
+    nombre: "Gorra Trendy",
+    precio: 35000,
+    imagen: "https://img.kwcdn.com/product/fancy/197f733b-6e92-42fb-90b2-1b68b6de1430.jpg?imageView2/2/w/800/q/70/format/avif",
+  },
+  {
+    id: 6,
+    nombre: "Reloj Moderno",
+    precio: 95000,
+    imagen: "https://img.kwcdn.com/product/fancy/094d36d2-7113-45a6-a04c-4baab27e3e95.jpg?imageView2/2/w/800/q/70/format/avif",
+  },
+
+{
+    id: 7,
+    nombre: "Reloj Moderno",
+    precio: 95000,
+    imagen: "https://img.kwcdn.com/product/fancy/afc4ba60-b2be-4bfa-95fc-473badb65bdd.jpg?imageView2/2/w/800/q/70/format/avif",
+  },
+
+{
+    id: 8,
+    nombre: "Reloj Moderno",
+    precio: 95000,
+    imagen: "https://img.kwcdn.com/product/fancy/2570c7c8-059b-461c-bb82-f98d4a4afee9.jpg?imageView2/2/w/800/q/70/format/avif",
+  },
+
+{
+    id: 9,
+    nombre: "Reloj Moderno",
+    precio: 95000,
+    imagen: "https://img.kwcdn.com/product/fancy/bb1984f8-2c38-4fed-8c37-3f1b5e794735.jpg?imageView2/2/w/800/q/70/format/avif",
+  },
+
+{
+    id: 10,
+    nombre: "Reloj Moderno",
+    precio: 95000,
+    imagen: "https://img.kwcdn.com/product/fancy/f3763d9a-9c6b-4cdf-85a3-2c1df9bc6338.jpg?imageView2/2/w/800/q/70/format/avif",
+  },
+
+{
+    id: 11,
+    nombre: "Reloj Moderno",
+    precio: 95000,
+    imagen: "https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/6456699e84d7fe63dbf5a1a523a58960.jpg?imageView2/2/w/800/q/70/format/avif",
+  },
+
+  {
+    id: 12,
+    nombre: "Reloj Moderno",
+    precio: 95000,
+    imagen: "https://img.kwcdn.com/product/fancy/e1fa2941-9d68-48db-bf57-fe7976aca870.jpg?imageView2/2/w/800/q/70/format/avif",
+  },
+
+  {
+    id: 13,
+    nombre: "Reloj Moderno",
+    precio: 95000,
+    imagen: "https://img.kwcdn.com/product/fancy/cc61bdd6-c00d-41ee-b0be-b04f3b040a0c.jpg?imageView2/2/w/800/q/70/format/avif",
+  },
+
+  {
+    id: 14,
+    nombre: "Reloj Moderno",
+    precio: 95000,
+    imagen: "https://img.kwcdn.com/product/fancy/733990a5-98d9-431a-9bde-5a109687f610.jpg?imageView2/2/w/800/q/70/format/avif",
+  },
+
+  {
+    id: 15,
+    nombre: "Reloj Moderno",
+    precio: 95000,
+    imagen: "https://img.kwcdn.com/product/fancy/36a01b37-7ce4-431e-b9db-8dff402d8d8f.jpg?imageView2/2/w/800/q/70/format/avif",
+  },
+
+  {
+    id: 16,
+    nombre: "Reloj Moderno",
+    precio: 95000,
+    imagen: "https://img.kwcdn.com/product/fancy/3d8537a9-f079-4405-b0ba-e2c6aec840e5.jpg?imageView2/2/w/800/q/70/format/avif",
+  },
+
+  {
+    id: 17,
+    nombre: "Reloj Moderno",
+    precio: 95000,
+    imagen: "https://img.kwcdn.com/product/fancy/871737d0-c384-40ce-9be2-901dca2582f0.jpg?imageView2/2/w/800/q/70/format/avif",
+  },
+
+  {
+    id: 18,
+    nombre: "Reloj Moderno",
+    precio: 95000,
+    imagen: "https://img.kwcdn.com/product/open/9b61b4ea9d5a4f779985877dcda874c5-goods.jpeg?imageView2/2/w/800/q/70/format/avif",
+  },
+
+  {
+    id: 19,
+    nombre: "Reloj Moderno",
+    precio: 95000,
+    imagen: "https://img.kwcdn.com/product/fancy/8855ec04-313b-4a89-94e1-2a6e4ac7592a.jpg?imageView2/2/w/800/q/70/format/avif",
+  },
+
+  {
+    id: 20,
+    nombre: "Reloj Moderno",
+    precio: 95000,
+    imagen: "https://img.kwcdn.com/product/open/b7d07f44d44d489ca70998f30f2a05a1-goods.jpeg?imageView2/2/w/800/q/70/format/avif",
+  },
+
+
+];
+
